9e03807267aa6805105507e723e7527ab16aa000
JI4JUN <ji4jun0097@gmail.com>
2025-04-30 11:44:28 +0800

    refactor: enhance TitleBar component structure and styling; add vertical separator and improve TitlebarContainer props for better layout management

--- src/renderer/components/business/title-bar/title-bar-container.tsx
+++ src/renderer/components/business/title-bar/title-bar-container.tsx
@@ -4,56 +4,75 @@ import { cn } from "@renderer/lib/utils";
 
 type Props = PropsWithChildren & HTMLAttributes<HTMLDivElement>;
 
+interface TitlebarProps extends Props {
+	className?: string;
+}
+
 const dragRegion = { WebkitAppRegion: "drag" } as React.CSSProperties;
 
-export function TitlebarContainer({ children, ...props }: Props) {
-  return (
-    <div
-      className={cn(
-        "flex w-full flex-row items-center border-b border-b-border bg-navbar",
-        "max-h-[var(--title-bar-height)] min-h-[var(--title-bar-height)]",
-        isMac ? "pl-[140px]" : "",
-      )}
-      style={dragRegion}
-      {...props}
-    >
-      {children}
-    </div>
-  );
+export function TitlebarContainer({ children, ...props }: TitlebarProps) {
+	return (
+		<div
+			className={cn(
+				"flex w-full flex-row items-center bg-navbar",
+				isMac ? "pl-[140px]" : "",
+			)}
+			style={dragRegion}
+			{...props}
+		>
+			{children}
+		</div>
+	);
 }
 
-export function TitlebarLeft({ children, ...props }: Props) {
-  return (
-    <div className="flex min-w-[275px] flex-row items-center px-2.5" {...props}>
-      {children}
-    </div>
-  );
+export function TitlebarLeft({ children, className, ...props }: TitlebarProps) {
+	return (
+		<div
+			className={cn(
+				"flex flex-row items-center h-[var(--title-bar-height)]",
+				className,
+			)}
+			{...props}
+		>
+			{children}
+		</div>
+	);
 }
 
-export function TitlebarCenter({ children, ...props }: Props) {
-  return (
-    <div
-      className={cn(
-        "flex flex-1 items-center font-bold",
-        isMac ? "px-5" : "px-0",
-      )}
-      {...props}
-    >
-      {children}
-    </div>
-  );
+export function TitlebarCenter({
+	children,
+	className,
+	...props
+}: TitlebarProps) {
+	return (
+		<div
+			className={cn(
+				"flex flex-1 items-center font-bold h-[var(--title-bar-height)]",
+				isMac ? "px-5" : "px-0",
+				className,
+			)}
+			{...props}
+		>
+			{children}
+		</div>
+	);
 }
 
-export function TitlebarRight({ children, ...props }: Props) {
-  return (
-    <div
-      className={cn(
-        "flex items-center justify-end pl-3",
-        isWindows ? "pr-[140px]" : "pr-3",
-      )}
-      {...props}
-    >
-      {children}
-    </div>
-  );
+export function TitlebarRight({
+	children,
+	className,
+	...props
+}: TitlebarProps) {
+	return (
+		<div
+			className={cn(
+				"flex items-center justify-end pl-3 h-[var(--title-bar-height)]",
+				isWindows ? "pr-[140px]" : "pr-3",
+				className,
+			)}
+			{...props}
+		>
+			{children}
+		</div>
+	);
 }
