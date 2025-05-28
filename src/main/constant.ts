2a8cd2764f4bd5434eeef421293b46e6ba7166b6
JI4JUN <ji4jun0097@gmail.com>
2025-05-28 17:18:35 +0800

    fix: fix the db error that macOS can't find the path of db

--- src/main/constant.ts
+++ src/main/constant.ts
@@ -1,4 +1,5 @@
-import pkg from "../../package.json";
+import { app } from 'electron';
+import path from 'path';
 
 export const isMac = process.platform === "darwin";
 export const isWin = process.platform === "win32";
@@ -7,9 +8,19 @@ export const isDev = process.env.NODE_ENV === "development";
 
 export const defaultLanguage = "zh-CN";
 
-export const APP_NAME = pkg.name;
+export const APP_NAME = 'ChatChat';
 export const DB_NAME = isDev ? "chatchat.dev.db" : "chatchat.db";
 export const DB_CONFIG = {
   dbFileName: DB_NAME,
   timeout: 30 * 1000,
 };
+
+export function getAppDataPath() {
+  if (process.platform === 'darwin') {
+    return path.join(app.getPath('userData'));
+  } else if (process.platform === 'win32') {
+    return path.join(process.env.APPDATA || '', APP_NAME);
+  } else {
+    return path.join(app.getPath('userData'));
+  }
+}
--- src/main/db/index.ts
+++ src/main/db/index.ts
@@ -7,41 +7,31 @@ import {
   drizzle,
 } from "drizzle-orm/better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
-import { app } from "electron";
-import { APP_NAME, DB_CONFIG } from "../constant";
+
+import { DB_CONFIG, getAppDataPath } from "../constant";
 import * as schema from "./schema";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const DB_PATH = path.join(
-  app.getPath("appData"),
-  APP_NAME,
-  DB_CONFIG.dbFileName
-);
-const sqlite = new Database(DB_PATH, {
+const dbPath = path.join(getAppDataPath(), DB_CONFIG.dbFileName);
+
+// Ensure the directory exists
+const dbDir = path.dirname(dbPath);
+if (!existsSync(dbDir)) {
+  mkdirSync(dbDir, { recursive: true });
+}
+
+const sqlite = new Database(dbPath, {
   timeout: DB_CONFIG.timeout,
 });
 
 export let db: BetterSQLite3Database<typeof schema>;
 
 export const dbConnect = async () => {
-  generateDbPath(DB_PATH);
   db = drizzle(sqlite, { schema });
 
   if (process.env.NODE_ENV === "production") {
     migrate(db, {
       migrationsFolder: path.join(__dirname, "../../../migrations"),
     });
-  }
-};
-
-const generateDbPath = (dirString: string) => {
-  try {
-    const dir = dirname(dirString);
-    if (!existsSync(dir)) {
-      mkdirSync(dir, { recursive: true });
-    }
-  } catch (error) {
-    console.error("Database connection error:", error);
-    throw error;
   }
 };
