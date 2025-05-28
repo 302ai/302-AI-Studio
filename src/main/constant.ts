941a88bc98ee7b4b5b3a57a7f4413b4bacda4ba8
JI4JUN <ji4jun0097@gmail.com>
2025-05-28 17:20:46 +0800

    refactor: update import statements

--- src/main/constant.ts
+++ src/main/constant.ts
@@ -1,5 +1,5 @@
-import { app } from 'electron';
-import path from 'path';
+import path from "node:path";
+import { app } from "electron";
 
 export const isMac = process.platform === "darwin";
 export const isWin = process.platform === "win32";
@@ -8,7 +8,7 @@ export const isDev = process.env.NODE_ENV === "development";
 
 export const defaultLanguage = "zh-CN";
 
-export const APP_NAME = 'ChatChat';
+export const APP_NAME = "ChatChat";
 export const DB_NAME = isDev ? "chatchat.dev.db" : "chatchat.db";
 export const DB_CONFIG = {
   dbFileName: DB_NAME,
@@ -16,11 +16,11 @@ export const DB_CONFIG = {
 };
 
 export function getAppDataPath() {
-  if (process.platform === 'darwin') {
-    return path.join(app.getPath('userData'));
-  } else if (process.platform === 'win32') {
-    return path.join(process.env.APPDATA || '', APP_NAME);
+  if (process.platform === "darwin") {
+    return path.join(app.getPath("userData"));
+  } else if (process.platform === "win32") {
+    return path.join(process.env.APPDATA || "", APP_NAME);
   } else {
-    return path.join(app.getPath('userData'));
+    return path.join(app.getPath("userData"));
   }
 }
