dd1a32d8f6ec304d6992f3da230a6d28f914c458
JI4JUN <ji4jun0097@gmail.com>
2025-05-28 17:46:19 +0800

    fix: setTitleBarOverlay only can be used in windows

--- src/main/services/window-service.ts
+++ src/main/services/window-service.ts
@@ -1,14 +1,20 @@
 import { titleBarOverlayDark, titleBarOverlayLight } from "@main/config";
 import { ThemeMode } from "@renderer/types/settings";
 import { BrowserWindow } from "electron";
+import { isWin } from "../constant";
 import { ServiceRegister } from "../shared/reflect";
 
 @ServiceRegister("windowService")
 export class WindowService {
   setTitleBarOverlay(theme: ThemeMode) {
+    // setTitleBarOverlay is only available on Windows
+    if (!isWin) {
+      return;
+    }
+
     BrowserWindow.getAllWindows().forEach((window) => {
       window.setTitleBarOverlay(
-        theme === ThemeMode.Dark ? titleBarOverlayDark : titleBarOverlayLight
+        theme === ThemeMode.Dark ? titleBarOverlayDark : titleBarOverlayLight,
       );
     });
   }
