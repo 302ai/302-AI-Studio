15b56ecf69090ea5b18dbcbf7be9fdd9291b3a6d
JI4JUN <ji4jun0097@gmail.com>
2025-05-29 12:43:29 +0800

    fix: fix the issue of titleBarOverlay in different os

--- src/main/services/window-service.ts
+++ src/main/services/window-service.ts
@@ -1,13 +1,13 @@
 import { titleBarOverlayDark, titleBarOverlayLight } from "@main/config";
+import { ThemeMode } from "@shared/types/settings";
 import { BrowserWindow } from "electron";
-import { ThemeMode } from "@/src/shared/types/settings";
 import { isWin } from "../constant";
 import { ServiceRegister } from "../shared/reflect";
 
 @ServiceRegister("windowService")
 export class WindowService {
   setTitleBarOverlay(theme: ThemeMode) {
-    // setTitleBarOverlay is only available on Windows
+    // * setTitleBarOverlay is only available on Windows
     if (!isWin) {
       return;
     }
