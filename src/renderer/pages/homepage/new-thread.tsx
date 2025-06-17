5d0ac533646f18c6a9eaf76d2d3655e88a31b010
JI4JUN <ji4jun0097@gmail.com>
2025-06-17 15:09:08 +0800

    style: modify newThread ui

--- src/renderer/pages/homepage/new-thread.tsx
+++ src/renderer/pages/homepage/new-thread.tsx
@@ -3,13 +3,13 @@ import { ChatInput } from "@renderer/components/business/chat-input";
 
 export function NewThread() {
   return (
-    <>
+    <div className="flex w-full flex-col items-center justify-center gap-6">
       <div className="flex flex-row items-center justify-center gap-4">
         <img src={logo} alt="logo" className="size-11" />
         <span className="text-4xl">Hello</span>
       </div>
 
       <ChatInput />
-    </>
+    </div>
   );
 }
