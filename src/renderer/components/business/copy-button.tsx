91cb8441775b143a5079a24a26ab749e6c654535
JI4JUN <ji4jun0097@gmail.com>
2025-06-20 09:45:02 +0800

    style: modify

--- src/renderer/components/business/copy-button.tsx
+++ src/renderer/components/business/copy-button.tsx
@@ -1,22 +1,20 @@
-import { useCopyToClipboard } from '@renderer/hooks/use-copy-to-clipboard'
-import { Check, Copy } from 'lucide-react'
-import { cn } from '@renderer/lib/utils'
-import { ButtonWithTooltip } from './button-with-tooltip'
-import { useTranslation } from 'react-i18next'
+import { useCopyToClipboard } from "@renderer/hooks/use-copy-to-clipboard";
+import { cn } from "@renderer/lib/utils";
+import { Check, Copy } from "lucide-react";
+import { useTranslation } from "react-i18next";
+import { ButtonWithTooltip } from "./button-with-tooltip";
 
 interface CopyButtonProps {
-  content: string
+  content: string;
 }
 
-export function CopyButton({
-  content
-}: CopyButtonProps) {
-  const { t } = useTranslation('translation', {
-    keyPrefix: 'common',
-  })
+export function CopyButton({ content }: CopyButtonProps) {
+  const { t } = useTranslation("translation", {
+    keyPrefix: "common",
+  });
   const { isCopied, handleCopy } = useCopyToClipboard({
     text: content,
-  })
+  });
 
   return (
     <ButtonWithTooltip
@@ -25,20 +23,20 @@ export function CopyButton({
       shape="square"
       className="relative size-8"
       onClick={handleCopy}
-      title={t('copy-to-clipboard')}
+      title={t("copy-to-clipboard")}
     >
       <Check
         className={cn(
           "absolute inset-0 m-auto h-4 w-4 transition-all duration-200 ease-in-out",
-          isCopied ? "scale-100 opacity-100" : "scale-0 opacity-0"
+          isCopied ? "scale-100 opacity-100" : "scale-0 opacity-0",
         )}
       />
       <Copy
         className={cn(
           "absolute inset-0 m-auto h-4 w-4 transition-all duration-200 ease-in-out",
-          isCopied ? "scale-0 opacity-0" : "scale-100 opacity-100"
+          isCopied ? "scale-0 opacity-0" : "scale-100 opacity-100",
         )}
       />
-    </ButtonWithTooltip >
-  )
+    </ButtonWithTooltip>
+  );
 }
