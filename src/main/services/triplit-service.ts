abf2371bad22d30e0f7c521056b180aef0c5720f
shiwo6324 <103474153+shiwo6324@users.noreply.github.com>
2025-06-27 15:49:54 +0800

    fix: 数据库恢复到v1

--- src/main/services/triplit-service.ts
+++ src/main/services/triplit-service.ts
@@ -133,7 +133,7 @@ export class TriplitService {
     const defaultDatabaseDir = path.join(userDataPath, "triplit");
     const defaultDatabaseFile = path.join(
       defaultDatabaseDir,
-      "appDb-v2.sqlite",
+      "appDb-v1.sqlite",
     );
 
     Logger.info("Default database file:", defaultDatabaseFile);
