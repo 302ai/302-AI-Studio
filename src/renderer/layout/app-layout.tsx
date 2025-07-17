e53269f8df6e8115ce2e1d45b466ab80e3d968bb
mthezi <mthezi@163.com>
2025-07-17 18:25:46 +0800

    fix: fetch before connect

--- src/renderer/layout/app-layout.tsx
+++ src/renderer/layout/app-layout.tsx
@@ -1,12 +1,15 @@
 import { AppSidebar } from "@renderer/components/business/sidebar";
 import { BasicTitleBar } from "@renderer/components/business/title-bar";
 import { Toast } from "@renderer/components/ui/toast";
+import { useAppShortcutListenser } from "@renderer/hooks/use-app-shortcut-listener";
 import { useShortcutsHandlers } from "@renderer/hooks/use-global-shortcuts";
 import { Outlet } from "react-router-dom";
 
 export function Layout() {
   // 初始化全局所有按键处理程序
   useShortcutsHandlers();
+  // 初始化仅限于APP内的按键监听
+  useAppShortcutListenser();
 
   return (
     <main className="flex h-screen w-screen flex-col overflow-hidden">
