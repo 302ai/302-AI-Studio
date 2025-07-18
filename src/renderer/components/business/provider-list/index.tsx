f663edc23bb4b37d0e193b576c760a80910cc252
shiwo6324 <103474153+shiwo6324@users.noreply.github.com>
2025-07-18 16:08:38 +0800

    style: adjust button width in ProviderList component

--- src/renderer/components/business/provider-list/index.tsx
+++ src/renderer/components/business/provider-list/index.tsx
@@ -393,7 +393,7 @@ export function ProviderList() {
           <div>{t("label")}</div>
           {providers.length > 0 ? (
             <Button
-              className=" w-[76px] shrink-0"
+              className=" w-[66px] shrink-0 "
               intent="primary"
               // onClick={() => setState({ type: "add" })}
               onClick={onClick}
